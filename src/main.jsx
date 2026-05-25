b9fc57b58a19e5335938951040bdf45fe49d8ace
gabriel <gabrielsim1023@gmail.com>
2026-05-25 10:55:06 +0800

    Add routing for generate page and clean up App

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import TicketGenerator from './TicketGenerator'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} /> {/* Scanning interface */}
+        <Route path="/generate" element={<TicketGenerator />} /> {/* Generation interface */}
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
